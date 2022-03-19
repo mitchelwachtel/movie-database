7d0a88bc668627c981b4a38a2c78e3a20151bc74
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-03-19 13:15:12 -0400

    created a route /reviews that sends back a JOIN table

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,24 +1,24 @@
 const reviews = require("express").Router();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "movie_db",
-  }
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "movie_db",
+});
 
 reviews.get("/", (req, res) => {
-  db.query("SELECT * FROM movies", function (err, results) {
-    if (err) {
+  db.query(
+    "SELECT m.id movie_id, m.movie_name, r.review, r.id review_id FROM movies m JOIN reviews r ON m.id = r.movie_id",
+    function (err, results) {
+      if (err) {
         console.error(err);
       } else {
         res.json(results);
       }
-  });
+    }
+  );
 });
-
 
 module.exports = reviews;
