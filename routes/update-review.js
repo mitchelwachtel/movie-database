a0e1ede4e915e9dc39d891c4dc13779966160b04
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-03-19 13:01:08 -0400

    update-review post works

--- routes/update-review.js
+++ routes/update-review.js
@@ -1,31 +1,34 @@
 const updateRev = require("express").Router();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "movie_db",
-  },
-  console.log(`movie_db`)
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "movie_db",
+});
 
 updateRev.post("/", (req, res) => {
   // TODO: update a movie review
-//   FIX THIS ... close though
-  const {review, movieId} = req.body;
+  const {review, reviewId} = req.body;
 
   db.query(
-    `INSERT INTO reviews(review, movie_id) VALUES('${review}', '${movieId}');`,
+    `UPDATE reviews SET review = '${review}' WHERE id = ${reviewId};`,
     function (err, results) {
       if (err) {
         console.error(err);
       } else {
-        res.json(results);
+        console.log("success");
       }
     }
   );
+  db.query(`SELECT * FROM reviews`, function (err, results) {
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 module.exports = updateRev;
